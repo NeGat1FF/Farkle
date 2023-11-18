6a6e639f411843165d3ae84ae2767a28f13a2541
NeGat1FF <worldalex1@gmail.com>
2023-11-18 20:20:36 +0200

    Add new game mode classes and player controller

---
+++ Source/Farkle/Public/FakrleSPGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FakrleSPGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FARKLE_API AFakrleSPGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
